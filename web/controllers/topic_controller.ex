6d0c468bd6560ff7be3c50221c4877f6ef8ebc13
Grant Nelson <grantmnelsn@gmail.com>
2017-04-25 00:59:34 -0400

    finished 117, only editing own topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,7 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:update, :edit, :delete]
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
@@ -11,7 +12,9 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" =>  topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+    changeset = conn.assigns[:user]
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
@@ -56,6 +59,19 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id }} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You Cannot Edit That")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 
 end
